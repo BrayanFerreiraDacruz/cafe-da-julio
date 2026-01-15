649eae5790a832e780a9e6b97af14d1bbadf9ea5
Brayan Ferreira Da Cruz <brayanferreiradacruz0@gmail.com>
2026-01-15 03:14:26 -0300

    fix: removendo dependencia fantasma e limpando build 13

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 // URL externa para garantir acesso do seu computador local ao Render
-const connectionString = "postgresql://cafe_admin:lfFL9vOmyPqGAKMlXFeRLTRWRzyuSgsq@dpg-d5k4kere5dus73a00l80-a.oregon-postgres.render.com/cafedajulio";
-
+const connectionString = "postgresql://cafe_admin:lfFL9vOmyPqGAKMlXFeRLTRWRzyuSgsq@dpg-d5k4kere5dus73a00l80-a.oregon-postgres.render.com/cafedajulio?sslmode=require";
 export default defineConfig({
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
